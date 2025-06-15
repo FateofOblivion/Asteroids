b5cdc041c5d74af1a2a23efd422799525935d5c7
FateofOblivion <steffonjones13@gmail.com>
2025-06-15 13:33:38 -0400

    done with updates

--- player.py
+++ player.py
@@ -6,8 +6,8 @@ class Player(CircleShape):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
 		# in the player class
-	def draw(self, Screen):
-		pygame.draw.polygon(Screen,(225,225,225),self.triangle(),2)
+	def draw(self, screen):
+		pygame.draw.polygon(screen,(225,225,225),self.triangle(),2)
 	
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
