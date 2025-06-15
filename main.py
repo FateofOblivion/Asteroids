da70d9e764e7a9a53aebf80fe2b9bfdfe02b34d6
FateofOblivion <steffonjones13@gmail.com>
2025-06-15 09:23:34 -0400

    based updated

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from constants import *
 import pygame
 def main():
-	print("Starting Asteroids!")
+	print(f"Starting Asteroids! \nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
     main()
